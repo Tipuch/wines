421bf1a4a770ac1e0325eff01e8d8891cf63d0c4
jp <fiorini751@gmail.com>
2018-12-16 13:10:17 -0500

    - removed queries file

--- queries.sql
+++
@@ -1,9 +0,0 @@
-SELECT saq.name, saq.country, saq.region, saq.designation_of_origin, saq.producer, saq.color, saq.price, reco.rating FROM saq_wines as saq
-JOIN wine_recommendations as reco ON 
-(reco.country = '' OR saq.country ILIKE reco.country) AND
-(reco.region = '' OR saq.region ILIKE reco.region) AND
-(reco.designation_of_origin = '' OR saq.designation_of_origin ILIKE reco.designation_of_origin) AND
-(reco.producer = '' OR saq.producer ILIKE reco.producer) AND
-(saq.color = reco.color) AND
-(reco.grape_variety = '' OR reco.grape_variety ILIKE ANY(saq.grape_varieties))
-ORDER BY ratio;
