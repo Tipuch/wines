9ca3884c2b7ab35f68e3abf159084fe2d2b32269
Tipuch <fiorini751@gmail.com>
2019-11-05 12:35:10 -0500

    - reduce the size of package.

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                exclude: /node_modules/,
                 use: 'babel-loader'
             },
             {
